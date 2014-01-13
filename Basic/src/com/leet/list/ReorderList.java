043e8e793c3fed027e0068598f826e25b39eee96
sohochaser <yanminglc@qq.com>
2014-01-13 14:14:23 +0800

    Reorder list done.
    Reorder list done.

--- Basic/src/com/leet/list/ReorderList.java
+++ Basic/src/com/leet/list/ReorderList.java
@@ -2,122 +2,162 @@ package com.leet.list;
 
 /**
  * <ol>
- * <li>
- * Given a singly linked list L: L0¡úL1¡ú¡­¡úLn-1¡úLn, reorder it to:
- * <li>L0¡úLn¡úL1¡úLn-1¡úL2¡úLn-2¡ú¡­
- * 
+ * <li>Given a singly linked list L: L0ï¿½ï¿½L1ï¿½ï¿½ï¿½Ln-1ï¿½ï¿½Ln, reorder it to:
+ * <li>L0ï¿½ï¿½Lnï¿½ï¿½L1ï¿½ï¿½Ln-1ï¿½ï¿½L2ï¿½ï¿½Ln-2ï¿½ï¿½
  * <li>You must do this in-place without altering the nodes' values.
- * 
  * <li>For example, Given {1,2,3,4}, reorder it to {1,4,2,3}.
- * 
  * </ol>
  * 
  * @author Allen
- * 
  */
 public class ReorderList {
-	public void reorderList(ListNode head) {
-		ListNode root = head;
+    public void reorderList(ListNode head) {
+        ListNode left = null;
+        ListNode right = null;
 
-		ListNode left = null;
-		ListNode right = null;
+        ListNode itr = head;
+        int n = 0;
+        while (itr != null) {
+            n++;
+            itr = itr.next;
+        }
 
-		ListNode oddList = null;
-		ListNode eList = null;
-		while (root != null) {
-			if (oddList == null) {
-				oddList = root;
-				left = oddList;
-			} else {
-				oddList.next = root;
-				
-			}
-			if (root.next != null) {
-				if (eList == null) {
-					eList = root.next;
-					right = eList;
-				} else {
-					eList.next = root.next;
-											
-				}				
-			} else {
-				break;
-			}
-			root = root.next.next;
-			
-		}
-		
-		pList(oddList);
-		pList(eList);
-		
-		if (root != null && root != oddList) {
-			oddList.next = root;
-			oddList = oddList.next;
-		}
-		oddList.next = null;
-		
-		pList(oddList);
-		pList(eList);
-		
-		root = right;
-		ListNode next;
-		ListNode pre = null;
-		while (root != null) {
-			next = root.next;
-			root.next = pre;
-			pre = root;
+        int leftEnd = n - (n / 2);
+        itr = head;
+        left = head;
+        n = 0;
+        while (itr != null) {
+            n++;
+            if (n == leftEnd) {
+                right = itr.next;
+                itr.next = null;
+                break;
+            }
+            itr = itr.next;
+        }
+     
+        itr = right;
+        ListNode next;
+        ListNode pre = null;
+        while (itr != null) {
+            next = itr.next;
+            itr.next = pre;
+            pre = itr;
+            itr = next;
+        }
 
-			root = next;
-		}
-		ListNode newHead = null;
+        ListNode itr1 = null;
+        while (left != null) {
+            if (itr1 == null) {
+                itr1 = left;
+                itr = itr1;
+            } else {
+                itr1.next = left;
+                itr1 = itr1.next;
+            }
+            left = left.next;
 
-		while (left != null && pre != null) {
-			if (newHead == null) {
-				newHead = left;
-				root = newHead;
-			} else {
-				root.next = left;
-				root = root.next;
-			}
-			left = left.next;
+            if (pre != null) {
+                itr1.next = pre;
+                itr1 = itr1.next;
+                pre = pre.next;
+            }
+        }
+        head = itr;
+    }
 
-			root.next = pre;
-			root = root.next;
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        b.next=null;
+        a=null;
+        pList(a);
+        new ReorderList().reorderList(a);
+        pList(a);
+    }
 
-			pre = pre.next;
-		}
-		if (left != null) {
-			if (root == null) {
-				root = left;
-			} else {
-				root.next = left;
-			}
-		}
-		if (root != null) {
-			root.next = null;
-		}
-		head = root;
-	}
+    public static void pList(final ListNode root) {
+        System.out.println("\n-----------------------------------------------");
+        ListNode head = root;
+        while (head != null) {
+            System.out.print(head.val + "\t");
+            head = head.next;
+        }
+    }
 
-	public static void main(String[] args) {
-		ListNode a = new ListNode(1);
-		ListNode b = new ListNode(2);
-		ListNode c = new ListNode(3);
-		ListNode d = new ListNode(4);
-		a.next = b;
-		b.next = c;
-		c.next = d;
-		pList(a);
-		new ReorderList().reorderList(a);
-		pList(a);
-	}
+    public void reorderListWrong(ListNode head) {
+        ListNode root = head;
 
-	public static void pList(ListNode root) {
-		System.out
-				.println("\n-----------------------------------------------\n");
-		while (root != null) {
-			System.out.print(root.val + "\t");
-			root = root.next;
-		}
-	}
+        ListNode left = null;
+        ListNode right = null;
+
+        ListNode oddList = null;
+        ListNode eList = null;
+        while (root != null) {
+            if (oddList == null) {
+                oddList = root;
+                root = root.next;
+                left = oddList;
+            } else {
+                oddList.next = root;
+                root = root.next;
+                oddList = oddList.next;
+            }
+            oddList.next = null;
+            if (root != null) {
+                if (eList == null) {
+                    eList = root;
+                    root = root.next;
+                    right = eList;
+                } else {
+                    eList.next = root;
+                    root = root.next;
+                    eList = eList.next;
+                }
+                eList.next = null;
+            } else {
+                break;
+            }
+        }
+
+        pList(left);
+        pList(right);
+
+        root = right;
+        ListNode next;
+        ListNode pre = null;
+        while (root != null) {
+            next = root.next;
+            root.next = pre;
+            pre = root;
+            root = next;
+        }
+
+        ListNode itr = null;
+        while (left != null) {
+            if (itr == null) {
+                itr = left;
+                root = itr;
+            } else {
+                itr.next = left;
+                itr = itr.next;
+            }
+            left = left.next;
+
+            if (pre != null) {
+                itr.next = pre;
+                itr = itr.next;
+                pre = pre.next;
+            }
+        }
+        head = root;
+    }
 }
---
+++ Basic/src/com/leet/list/SortList.java
@@ -0,0 +1,16 @@
+package com.leet.list;
+
+/**
+ * <ol>
+ * <li>Sort a linked list in O(n log n) time using constant space complexity.
+ * </ol>
+ * <p>
+ * Created on Jan 13, 2014
+ * 
+ * @author Allen Yan
+ */
+public class SortList {
+    public ListNode sortList(ListNode head) {
+        return head;
+    }
+}
