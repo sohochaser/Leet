41bc6261aeb99c4d162db28c1ff68e93d0abb306
sohochaser <yanminglc@qq.com>
2014-01-21 13:34:21 +0800

    Permutation DP failed.

--- Basic/src/com/leet/nb/Permutations.java
+++ Basic/src/com/leet/nb/Permutations.java
@@ -1,6 +1,8 @@
 package com.leet.nb;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.leet.Notice;
 
@@ -18,49 +20,63 @@ import com.leet.Notice;
  */
 public class Permutations {
 
+    // !!! Negative value like -1.
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         if (num == null || num.length == 0) {
             return result;
-        } else if (num.length == 1) {
-            ArrayList<Integer> pre = new ArrayList<Integer>();
-            pre.add(num[0]);
-            result.add(pre);
-        } else if (num.length == 2) {
-            ArrayList<Integer> pre = new ArrayList<Integer>();
-            pre.add(num[0]);
-            calculate(result, pre, num, 0);
-            pre.clear();
-            pre.add(num[1]);
-            calculate(result, pre, num, 1);
-        } else {
-            ArrayList<Integer> pre = new ArrayList<Integer>();
-            calculateR(result, pre, num, -1);
+        }
+        if (num.length == 1) {
+            ArrayList<Integer> iList = new ArrayList<Integer>();
+            iList.add(num[0]);
+            result.add(iList);
+            return result;
+        }
+
+        ArrayList<ArrayList<String>> tmp = new ArrayList<ArrayList<String>>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < num.length; i++) {
+            tmp.add(new ArrayList<String>());
+            map.put("" + num[i], num[i]);
+        }
+        calculate(result, num, num.length - 1, tmp);
+        ArrayList<String> p = tmp.get(num.length - 1);
+
+        for (String s : p) {
+            System.out.println(s);
+            ArrayList<Integer> iList = new ArrayList<Integer>();
+            for (int i = 0; i < s.length(); i++) {
+                iList.add(map.get(String.valueOf(s.charAt(i))));
+            }
+            result.add(iList);
         }
         return result;
     }
 
-    @Notice(desc = "Initial value.")
-    private void calculate(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> pre, int[] num, int selected) {
-        int[] flag = new int[num.length];
-        ArrayList<Integer> r = new ArrayList<Integer>();
-        for (int i = 0; i < num.length; i++) {
-            flag[i] = i;
-            r.add(num[i]);
-        }
-        result.add(r);
-        while (true) {
-            int[] mark = new int[num.length];
-            for (int i = 0; i < num.length; i++) {
-                mark[i] = 0;
+    private void calculate(ArrayList<ArrayList<Integer>> result, int[] num, int pos, ArrayList<ArrayList<String>> tmp) {
+        if (pos == 0) {
+            ArrayList<String> cur = new ArrayList<String>();
+            cur.add("" + num[0]);
+            tmp.set(0, cur);
+        } else {
+            if (tmp.get(pos - 1).size() == 0) {
+                calculate(result, num, pos - 1, tmp);
             }
-            r = new ArrayList<Integer>();
-            for (int i = 0; i < num.length; i++) {
-                if (mark[i] == 0) {
-                    mark[i] = 1;
-                    flag[i] = flag[i] + 1;
+            ArrayList<String> pre = tmp.get(pos - 1);
+            ArrayList<String> cur = new ArrayList<String>();
+            String single = String.valueOf(num[pos]);
+            StringBuilder sb = new StringBuilder();
+            for (String str : pre) {
+                sb.setLength(0);
+                for (int i = 0; i < str.length(); i++) {
+                    sb.append(str.substring(0, i)).append(single).append(str.substring(i));
+                    cur.add(sb.toString());
+                    sb.setLength(0);
                 }
+                sb.append(str).append(single);
+                cur.add(sb.toString());
             }
+            tmp.set(pos, cur);
         }
     }
 
@@ -123,8 +139,8 @@ public class Permutations {
     }
 
     public static void main(String[] args) {
-        int[] num = new int[] { 1, 2, 3, 4 };
-        ArrayList<ArrayList<Integer>> result = new Permutations().permuteR(num);
+        int[] num = new int[] { 0, -1, 1 };
+        ArrayList<ArrayList<Integer>> result = new Permutations().permute(num);
         for (ArrayList<Integer> r : result) {
             for (Integer i : r) {
                 System.out.print(i + "\t");
