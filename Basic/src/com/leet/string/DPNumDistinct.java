598cbd7c2c56907d8a8cfa7c431e5e5eeb2dad5e
sohochaser <yanminglc@qq.com>
2014-01-27 22:15:33 +0800

    dp
    add comment

--- Basic/src/com/leet/string/DPNumDistinct.java
+++ Basic/src/com/leet/string/DPNumDistinct.java
@@ -1,5 +1,24 @@
 package com.leet.string;
 
+/**
+ * <ol>
+ * <li>Given a string S and a string T, count the number of distinct
+ * subsequences of T in S.
+ * 
+ * <li>A subsequence of a string is a new string which is formed from the
+ * original string by deleting some (can be none) of the characters without
+ * disturbing the relative positions of the remaining characters. (ie, "ACE" is
+ * a subsequence of "ABCDE" while "AEC" is not).
+ * 
+ * <li>Here is an example:
+ * <li>S = "rabbbit", T = "rabbit"
+ * 
+ * <li>Return 3
+ * </ol>
+ * 
+ * @author Allen
+ * 
+ */
 public class DPNumDistinct {
 	public int numDistinct(String s, String t) {
 		int[][] dp = new int[t.length() + 1][s.length() + 1];
@@ -15,7 +34,7 @@ public class DPNumDistinct {
 			char c = t.charAt(i - 1);
 			for (int j = 1; j <= s.length(); j++) {
 				dp[i][j] = dp[i][j - 1];
-				if (c == s.charAt(j-1)) {
+				if (c == s.charAt(j - 1)) {
 					dp[i][j] += dp[i - 1][j - 1];
 				}
 			}
